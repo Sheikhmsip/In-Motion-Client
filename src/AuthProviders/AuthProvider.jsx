88f5e415c391d21eb3d3013edd38506dcf1c2d8e
Sheikhmsip <sheikhmsip@gmail.com>
2023-06-10 14:33:47 +0600

    added JWT and create useAuth and useAxiosSecure hook

--- src/AuthProviders/AuthProvider.jsx
+++ src/AuthProviders/AuthProvider.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useEffect } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
-// import axios from "axios"
+import axios from "axios"
 
 const auth = getAuth(app)
 export const AuthContext = createContext(null);
@@ -43,20 +43,20 @@ const AuthProvider = ({children}) => {
     useEffect( () =>{
        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
-            console.log("current User", currentUser);
+            // console.log("current User", currentUser);
 
             // get and set token 
-        //    if(currentUser){
-        //     axios.post('https://bistro-boss-server-three-theta.vercel.app/jwt', {email: currentUser.email})
-        //     .then(data => {
-        //         // console.log(data.data.token)
-        //         localStorage.setItem('access-token', data.data.token)
-        //         setLoading(false);
-        //     })
-        //    }
-        //    else{
-        //     localStorage.removeItem('access-token')
-        //    }
+           if(currentUser){
+            axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+            .then(data => {
+                // console.log(data.data.token)
+                localStorage.setItem('access-token', data.data.token)
+                setLoading(false);
+            })
+           }
+           else{
+            localStorage.removeItem('access-token')
+           }
 
 
             
